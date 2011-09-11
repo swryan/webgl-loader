730fcead7a5dd1c22e4fb6d6fd42640b7caaec4d
wonchun <wonchun@cf1d3882-7a70-f5af-399d-f633c99e018c>
2011-09-11 03:22:35 +0000

    Test for OBJ file parsing.

--- bin/build.sh
+++ bin/build.sh
@@ -3,3 +3,4 @@
 ../src/objcompress.cc
 ../src/testing/all_codepoints.cc
 ../src/testing/good_codepoints.cc
+../src/testing/wavefront_obj_file_test.cc
--- bin/clean.sh
+++ bin/clean.sh
@@ -3,3 +3,4 @@ rm -f objanalyze
 rm -f objcompress
 rm -f all_codepoints
 rm -f good_codepoints
+rm -f wavefront_obj_file_test
